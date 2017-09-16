6273f5afadf6024487664b9e05ced638b1d26fe5
sumrukars <sumru.kars@ceng.deu.edu.tr>
2017-09-16 15:51:40 +0300

    ilk versiyon

---
+++ app/src/main/java/com/example/asus/turkcell/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.asus.turkcell;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+
+    }
+
+
+
+
+}
